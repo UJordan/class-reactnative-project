a5ddd6637604d6d62e21eba0bd22f018eff4987a
JordanU <42947912+UJordan@users.noreply.github.com>
2023-02-28 15:28:25 -0800

    Gestures Part 1

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,24 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, PanResponder, Alert } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { baseUrl } from '../../shared/baseUrl';
 import * as Animatable from 'react-native-animatable'
 
 const RenderCampsite = (props) => {
     const { campsite } = props;
+
+    const isLeftSwipe = ({ dx }) => dx < -200;
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderEnd: (e, gestureState) => {
+            console.log('pan responder end', gestureState);
+            if (isLeftSwipe(gestureState)) {
+                Alert.alert(
+                    'Add Favorite',
+                    'Are you sure you wish to add ' +
+                        campsite.name +
+                        ' to favorites?',
+                    [
+                        {
+                            text: 'Cancel',
+                            style: 'cancel',
+                            onPress: () => console.log('Cancel Pressed')
+                        },
+                        {
+                            text: 'OK',
+                            onPress: () =>
+                                props.isFavorite
+                                    ? console.log('Already set as a favorite')
+                                    : props.markFavorite()
+                        }
+                    ],
+                    { cancelable: false }
+                );
+            }
+        }
+    });
+
     if (campsite) {
         return (
             <Animatable.View
                 animation='fadeInDownBig'
                 duration={2000}
                 delay={1000}
+                {...panResponder.panHandlers}
             >
                 <Card containerStyle={styles.cardContainer}>
                     <Card.Image source={{ uri: baseUrl + campsite.image }}>
                         <View style={{ justifyContent: 'center', flex: 1 }}>
-                            <Text
-                                style={styles.cardText}
-                            >
-                            </Text>
+                            <Text style={styles.cardText}>{campsite.name}</Text>
                         </View>
                     </Card.Image>
                     <Text style={{ margin: 20 }}>{campsite.description}</Text>
@@ -41,7 +72,7 @@ const RenderCampsite = (props) => {
                             color='#5637DD'
                             raised
                             reverse
-                            onPress={() => props.onShowModal()}
+                            onPress={props.onShowModal}
                         />
                     </View>
                 </Card>
@@ -71,7 +102,7 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         color: 'white',
         fontSize: 20
-    },
+    }
 });
 
 export default RenderCampsite;
